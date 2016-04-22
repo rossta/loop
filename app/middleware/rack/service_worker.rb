10ba39cea4904b459efeeb3a052823de92bce7e3
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-22 15:48:20 -0400

    Implement production strategy for Rack::ServiceWorker

--- app/middleware/rack/service_worker.rb
+++ app/middleware/rack/service_worker.rb
@@ -5,45 +5,54 @@ module Rack
     end
 
     def call(env)
-      "#{self.class}::Handler".constantize.new(@app, *@args).call(env)
+      case env['REQUEST_METHOD']
+      when 'GET', 'HEAD'
+        path = env['PATH_INFO'].chomp('/')
+        return respond_to(path, env) if match?(path)
+      end
+
+      @app.call(env)
     end
 
-    class Handler
-      def initialize(app)
-        @app = app
+    def match?(path)
+      path == '/serviceworker.js'
+    end
+
+    private
+
+    def respond_to(path_info, env)
+      if config.compile
+        log "Compiling #{path_info} from Sprockets"
+        sprockets_server.call(env)
+      else
+        file_path = asset_path(path_info)
+        log "Proxing #{path_info} from #{file_path}"
+        file_server.call(env.merge('PATH_INFO' => file_path))
       end
+    end
 
-      def match?(path)
-        path == '/serviceworker.js'
-      end
+    def log(msg)
+      logger.info "[Rack::ServiceWorker] - #{msg}"
+    end
 
-      def call(env)
-        case env['REQUEST_METHOD']
-        when 'GET', 'HEAD'
-          path = env['PATH_INFO'].chomp('/')
-          return respond_with(path, env) if match?(path)
-        end
+    def logger
+      Rails.logger
+    end
 
-        @app.call(env)
-      end
+    def sprockets_server
+      Rails.application.assets
+    end
 
-      def respond_with(path, env)
-        Rails.logger.info "Rack::ServiceWorker #{path}"
+    def file_server
+      @file_server ||= Rack::File.new(Rails.root.join("public"))
+    end
 
-        if config.compile
-          sprockets_environment.call(env.merge('PATH_INFO' => path))
-        else
-          raise "Add support for precompiled serviceworker"
-        end
-      end
+    def config
+      Rails.application.config.assets
+    end
 
-      def sprockets_environment
-        Rails.application.assets
-      end
-
-      def config
-        Rails.application.config.assets
-      end
+    def asset_path(path)
+      ActionController::Base.helpers.asset_path(path.gsub(/^\//, ''))
     end
   end
 end
