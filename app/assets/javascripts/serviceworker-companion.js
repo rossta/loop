939c78cf51635ca5f1c4e3afd1a0098fc79edd52
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-25 22:05:43 -0400

    Bump serviceworker scripts

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,34 +1,37 @@
 (function() {
   function register() {
-    if (navigator.serviceWorker) {
-      log('Registering service worker...');
-    } else {
-      log('No browser support for service worker');
-      return;
-    }
-
-    return navigator.serviceWorker.register('/serviceworker.js').then(onRegistration);
+    navigator.serviceWorker.register('/serviceworker.js').then(onRegistration);
   }
 
   function onRegistration(registration) {
     if (!navigator.serviceWorker.controller) {
       log('No controller');
-      return;
     }
 
     if (registration.waiting) {
       log('waiting', registration.waiting);
-      return;
+      registration.waiting.addEventListener('statechange', onStateChange('waiting'));
     }
 
     if (registration.installing) {
       log('installing', registration.installing);
-      return;
+      registration.installing.addEventListener('statechange', onStateChange('installing'));
+    }
+
+    if (registration.active) {
+      log('active', registration.active);
+      registration.active.addEventListener('statechange', onStateChange('active'));
     }
 
     registration.addEventListener('updatefound', function() {
       log('updatefound', registration.installing);
     });
+  }
+
+  function onStateChange(initialState) {
+    return function(e) {
+      log('statechange', 'initial state', initialState, 'changed to', e.target.state);
+    }
   }
 
   function log() {
